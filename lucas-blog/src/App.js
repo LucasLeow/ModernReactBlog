ffc7bebfc50a20e2eb94ceda28f0a075ca23837c
Lucas Leow <leowjianhoong@gmail.com>
2023-07-08 22:35:09 +0800

    Created Navbar & Home components

--- lucas-blog/src/App.js
+++ lucas-blog/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Home";
-  const likes = 50;
   return (
     <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p> Liked {likes} times </p>
-      </div>
+      <Navbar />
+      <Home />
     </div>
   );
 }
