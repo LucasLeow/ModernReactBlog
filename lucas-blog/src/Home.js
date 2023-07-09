1c5fe922809e727a3f9c164b87f9923c708569fb
Lucas Leow <leowjianhoong@gmail.com>
2023-07-09 15:53:09 +0800

    Completed React Event handler

--- lucas-blog/src/Home.js
+++ lucas-blog/src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("test");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello ", name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(ev)=>{
+                console.log(ev); // or can pass to handleClickAgain fn to be accessed
+                handleClickAgain("mario")
+                }}>Click me again</button>
         </div>
     );
 }
