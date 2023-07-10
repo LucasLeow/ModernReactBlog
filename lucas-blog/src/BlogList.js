4a431020417b4ebe2fb151a2faccc2cfe1fe9a8f
Lucas Leow <leowjianhoong@gmail.com>
2023-07-10 13:16:40 +0800

    Implemented data fetch & removed delete functionality

--- lucas-blog/src/BlogList.js
+++ lucas-blog/src/BlogList.js
@@ -7,7 +7,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
                 <div className="blog-preview" key={item.id}>
                     <h2>{ item.title }</h2>
                     <p>Written by { item.author }</p>
-                    <button onClick={()=>{handleDelete(item.id)}}>Delete</button>
                 </div>
             ))}
         </div>
--- lucas-blog/src/Home.js
+++ lucas-blog/src/Home.js
@@ -2,25 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [ blogs, setBlogs ] = useState([
-        {title: "Blog 1", body: "Duis commodo tempor officia.", author: "mario", id: 1},
-        {title: "Blog 2", body: "Duis  tempor officia.", author: "yoshi", id: 2},
-        {title: "Blog 3", body: " commodo tempor officia.", author: "luigi", id: 3}
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(item => item.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [ blogs, setBlogs ] = useState(null);
 
     useEffect(() => {
-        console.log("useEffect ran");
-    })
+        fetch("http://localhost:8000/blogs")
+        .then(res=>res.json())
+        .then((data)=>{
+            console.log(data);
+            setBlogs(data);
+        })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter(item => item.author === "mario")} title={"Mario's Blogs"}/>
+            {blogs && <BlogList blogs={blogs} title={"All Blogs"}/>}
+            {blogs && <BlogList blogs={blogs.filter(item => item.author === "mario")} title={"Mario's Blogs"}/>}
         </div>
     );
 }
