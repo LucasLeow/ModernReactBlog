39a7c6842f0d0eefc702a0e5f1b5b91559a4c1e4
Lucas Leow <leowjianhoong@gmail.com>
2023-07-11 01:32:52 +0800

    Implemented React Router

--- lucas-blog/src/App.js
+++ lucas-blog/src/App.js
@@ -1,12 +1,19 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar />
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                </Routes>
+            </div>
+        </div>
+    </Router>
   );
 }
 
