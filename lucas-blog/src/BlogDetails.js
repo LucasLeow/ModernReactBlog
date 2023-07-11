9b7c7d90deed77db9782e463afaa8b7b396486a2
Lucas Leow <leowjianhoong@gmail.com>
2023-07-11 14:09:17 +0800

    Completed delete feature

--- lucas-blog/src/BlogDetails.js
+++ lucas-blog/src/BlogDetails.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data, pending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+    const navigate = useNavigate();
+
+    const handleClick =() => {
+        fetch(`http://localhost:8000/blogs/${data.id}`, {
+            method: "DELETE"
+        })
+        .then(()=>{
+            navigate("/");
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
                     <h2> { data.title } </h2>
                     <p> Written by { data.author }</p>
                     <p> { data.body }</p>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
